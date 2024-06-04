9c3e7c3860b03f0fa6ca452be88e428f71c4a87f
avatar-lavventura <alper.alimoglu@gmail.com>
2024-06-04 20:00:04 +0300

    Fix some bugs, minor improvements, and more

--- contracts/AutonomousSoftwareOrg.sol
+++ contracts/AutonomousSoftwareOrg.sol
@@ -37,7 +37,6 @@ contract AutonomousSoftwareOrg {
         uint requestedFund;
         uint deadline;
         uint voteCount;
-        bool withdrawn;
         mapping(address => bool) voted;
     }
 
@@ -204,7 +203,6 @@ contract AutonomousSoftwareOrg {
         _proposal.requestedFund = requestedFund;
         _proposal.deadline = deadline;
         _proposal.voteCount = 0;
-        _proposal.withdrawn = false;
         emit LogPropose(proposals.length, title, url, requestedFund, deadline);
     }
 
@@ -223,12 +221,9 @@ contract AutonomousSoftwareOrg {
         uint fund = proposals[propNo].requestedFund;
         weiBalance -=  fund;
         proposals[propNo].requestedFund = 0;
-        if (proposals[propNo].withdrawn == true) {
-            revert();
-        }
-        payable(msg.sender).transfer(fund);
-        proposals[propNo].withdrawn = true;
-        emit LogWithdrawProposalFund(propNo,proposals[propNo].requestedFund, block.number, msg.sender);
+        (bool success, ) = msg.sender.call{value: fund}("");
+        require(success, "Transfer failed.");
+        emit LogWithdrawProposalFund(propNo, fund, block.number, msg.sender);
     }
 
     function BecomeMemberCandidate(string memory url) public
@@ -396,13 +391,12 @@ contract AutonomousSoftwareOrg {
     }
 
     function getProposal(uint propNo)
-        public view returns (string memory, string memory, bytes32, uint, uint, bool, uint) {
+        public view returns (string memory, string memory, bytes32, uint, uint, uint) {
         return (proposals[propNo].title,
                 proposals[propNo].url,
                 proposals[propNo].propHash,
                 proposals[propNo].requestedFund,
                 proposals[propNo].deadline,
-                proposals[propNo].withdrawn,
                 proposals[propNo].voteCount);
     }
 
